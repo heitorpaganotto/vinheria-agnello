204d3b475fbc2a1c160d18ca22e4ad4bff3b1073
Heitor Paganotto <paganottoheitor079@gmail.com>
2025-04-14 15:03:19 -0300

    sincronizacao de js com o html e features no css

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+// // Exibe o alerta de boas-vindas
+// alert("Seja Bem-vindo à Vinheria Agnello");
+
+// // Declaração das variáveis para email e senha
+// var email = "";
+// var senha = "";
+
+// // Loop para garantir que o email e a senha não sejam vazios
+// while (email === "" || senha === "") {
+//     email = prompt("Digite seu email para login");
+//     if (email === "") {
+//         alert("Campo de email não pode ser vazio!");
+//         continue;
+//     }
+
+//     senha = prompt("Digite sua senha para login");
+//     if (senha === "") {
+//         alert("Campo de senha não pode ser vazio!");
+//     }
+// }
+
+// // Alerta informando que o cadastro foi realizado
+// alert("Cadastro realizado! Veja os detalhes no console.");
+
+// // Exibe o email diretamente na página HTML
+// document.getElementById("email-display").textContent = email;
+
+// // Exibe os detalhes no console
+// console.log("Email: " + email);
+// console.log("Senha: " + senha);
+// console.log("Cadastro realizado com sucesso!");
+// console.log("Seja Bem-vindo à Vinheria Agnello!");
+// console.log("Aproveite nossas opções de vinhos!");
